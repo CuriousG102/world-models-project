f207f2cf24b581e41bd7db588327dda31b240d3b
Miles Hutson <mileshutson@gmail.com>
2019-12-01 02:45:02 -0600

    Add code for loading and inspecting data from Doom environment and fix very bad bug in data collection code this allowed me to spot.

---
+++ examine_doom_data.py
@@ -0,0 +1,50 @@
+""" Some data examination """
+import numpy as np
+from matplotlib import cm
+import matplotlib.pyplot as plt
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Dataset examination')
+parser.add_argument('--datasets', type=str, default='datasets',
+                    help='Where the datasets are stored')
+parser.add_argument('--example_num', type=int)
+
+args = parser.parse_args()
+
+
+def plot_rollout():
+    """ Plot a rollout """
+    from torch.utils.data import DataLoader
+    from data.loaders import VariableLengthRolloutSequenceDataset
+    dataloader = DataLoader(
+        VariableLengthRolloutSequenceDataset(
+            root=args.datasets, transform=lambda x: x.transpose((0,2,3,1)), 
+            buffer_size=10, seq_len=500, train=False),
+        batch_size=1)
+
+    # setting up subplots
+    plt.subplot(2, 2, 1)
+    monitor_obs = plt.imshow(np.zeros((64, 64, 3)))
+    plt.subplot(2, 2, 2)
+    monitor_next_obs = plt.imshow(np.zeros((64, 64, 3)))
+    plt.subplot(2, 2, 3)
+    monitor_diff = plt.imshow(np.zeros((64, 64, 3)))
+
+    for i, data in enumerate(dataloader):
+        if i != args.example_num:
+            continue
+        obs_seq = data[0].numpy().squeeze()
+        action_seq = data[1].numpy().squeeze()
+        next_obs_seq = data[-1].numpy().squeeze()
+        for obs, action, next_obs in zip(obs_seq, action_seq, next_obs_seq):
+            monitor_obs.set_data(obs.astype(np.uint8))
+            monitor_next_obs.set_data(next_obs.astype(np.uint8))
+            monitor_diff.set_data(next_obs - obs)
+            print(action)
+            plt.pause(.01)
+        break
+
+if __name__ == '__main__':
+    plot_rollout()
